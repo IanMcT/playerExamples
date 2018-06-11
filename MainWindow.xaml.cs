b3b5bc371f57a1a98240da610c2c20f8cb0f929e
IanMcT <ian.mctavish@tldsb.on.ca>
2018-06-11 14:30:02 -0400

    Add files via upload

---
+++ MainWindow.xaml.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace CoolInheritance
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        List<Player> players;
+//        Player player;
+        public MainWindow()
+        {
+            InitializeComponent();
+            players = new List<Player>();
+            players.Add(new Player(canvas));
+            players.Add(new Player(canvas));
+        }
+
+        private void Window_MouseUp(object sender, MouseButtonEventArgs e)
+        {
+            for (int i = 0; i < players.Count; i++)
+            {
+                players[i].move();
+            }
+
+            for (int i = players.Count - 1; i >= 0; i--) {
+                if (players[i].getLeft() >= 450) {
+                    players[i].destroy();
+                    players.RemoveAt(i);
+
+                }
+            }
+            //player.move();
+        }
+    }
+}
---
+++ Player.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Controls;
+using System.Windows.Media;
+using System.Windows.Shapes;
+
+namespace CoolInheritance
+{
+    internal class Player 
+    {
+        private Rectangle rectangle;
+        private Canvas canvas;
+        private static int itemCount = 0;
+
+        public Player(Canvas c)
+        {
+            canvas = c;
+            rectangle = new Rectangle();
+            rectangle.Width = 100;
+            rectangle.Height = 100;
+            rectangle.Fill = Brushes.HotPink;
+            canvas.Children.Add(rectangle);
+            Canvas.SetLeft(rectangle, itemCount * 200+50);
+            itemCount++;
+        }
+
+        public void move() {
+            double pos = Canvas.GetLeft(this.rectangle);
+            Canvas.SetLeft(rectangle, pos + 100);
+
+            
+        }
+
+        public double getLeft() {
+            return Canvas.GetLeft(this.rectangle);
+        }
+
+        public void destroy() {
+            canvas.Children.Remove(rectangle);
+        }
+    }
+}
